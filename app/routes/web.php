862aa2d8d112d06d9c9a4f01bfe99ff943d88aa0
WaveHack <email@wavehack.net>
2017-03-05 23:56:33 +0100

    Update routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/** @var \Illuminate\Routing\Router $router */
 
-Route::get('/', function () {
+$router->get('/', function () {
+
+    $registrations = \App\Models\Registration::all();
+
+    dd($registrations);
+
     return view('welcome');
 });
