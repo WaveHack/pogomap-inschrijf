c81e9533875a3f7b7bf2c64eac7406450605bb20
WaveHack <email@wavehack.net>
2017-03-06 00:22:14 +0100

    Working on views

--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -1,11 +1,19 @@
 <!doctype html>
 <html lang="nl">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Inschrijf</title>
+
+    <link rel="stylesheet" href="{{ mix('assets/app/css/app.css') }}">
 </head>
 <body>
 
 @yield('content')
+
+<script src="{{ mix('assets/app/js/app.js') }}"></script>
 
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
-const { mix } = require('laravel-mix');
+const {mix} = require('laravel-mix');
 
 mix.setPublicPath('public');
 
 mix.js('app/resources/assets/js/app.js', 'public/assets/app/js')
-   .sass('app/resources/assets/sass/app.scss', 'public/assets/app/css');
+    .sass('app/resources/assets/sass/app.scss', 'public/assets/app/css')
+    .version();
