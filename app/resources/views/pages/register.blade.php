5fdacc0a4718d74fb4581b7304b8f2eb2298f5f0
WaveHack <email@wavehack.net>
2017-03-06 18:11:17 +0100

    Update registration form text

--- app/resources/views/pages/register.blade.php
+++ app/resources/views/pages/register.blade.php
@@ -27,18 +27,21 @@
 
         <div class="form-group">
             <div class="col-sm-9 col-sm-offset-3">
-                <p>Na goedkeuring krijg je op dit adres een email met je wachtwoord. Dit kan enige tijd duren aangezien dit handmatig gebeurt. Ook als je je wachtwoord vergeten bent kun je hiermee een nieuw wachtwoord aanvragen.</p>
-                <p>Je in-game naam en email zijn NIET zichtbaar voor andere gebruikers, anders dan voor de beheerders van de Pokémon Go map van Groningen ter controle. Je email-adres wordt ook niet verstrekt aan derden of voor promotiedoeleinden gebruikt.</p>
+                <p>Na goedkeuring krijg je op dit adres een email met je wachtwoord. Dit kan enige tijd duren aangezien dit handmatig gebeurt. Als je een van de beheerders kent dan kun je hen ook vragen.</p>
+                <p>Als je je wachtwoord vergeten bent kun je <a href="#">een nieuw wachtwoord aanvragen</a>.</p>
+                <p>Je in-game naam en email zijn NIET zichtbaar voor andere gebruikers, anders dan voor de beheerders van de Pokémon Go map van Groningen ter controle. Je email-adres wordt ook NIET verstrekt aan derden en wordt NIET voor promotiedoeleinden gebruikt.</p>
             </div>
         </div>
 
         <div class="form-group">
             <label for="buddy_file" class="col-sm-3 control-label">Verificatie <span class="text-danger">*</span></label>
             <div class="col-sm-9">
-                Stap 1: Geef je buddy de naam <em>{{ $buddy_name }}</em><br>
-                Stap 2: Maak een screenshot van je character, zorg dat je naam, je buddy naam en 'start date' duidelijk zichtbaar zijn en voeg dit bestand toe:<br><br>
+                Stap 1: Geef je buddy de naam <mark style="font-family: monospace;">{{ $buddy_name }}</mark><br>
+                Stap 2: Maak een screenshot van je character<br>
+                Stap 3: Zorg dat je in-game naam, je buddy naam, je level, experience en start date duidelijk zichtbaar zijn<br>
+                Stap 4: Voeg dit bestand hier toe in de originele resolutie:<br><br>
                 <input type="file" name="buddy_file" class="custom-file-input" id="buddy_name" required><br>
-                Voorbeeld;<br>
+                Voorbeeld (lage resolutie):<br>
                 <img src="{{ asset('assets/app/images/registration_example.png') }}" alt="Voorbeeld" class="img-responsive img-thumbnail">
             </div>
         </div>
@@ -48,11 +51,12 @@
             <div class="col-sm-9">
                 <p>Door je in te schrijven verklaar je akkoord te gaan met de volgende voorwaarden:</p>
                 <ul>
-                    <li>Na goedkeuring zul je je accountgegevens niet delen. Hier wordt geanonimiseerd op gecontroleerd door de beheerders en bij constatering van misbruik wordt het account gedeactiveerd.</li>
+                    <li>Je zal je accountgegevens niet delen met anderen om hen toegang tot de Pokémon Go map van Groningen te geven met jouw account.</li>
+                    <li>Er wordt hier geanonimiseerd op gecontroleerd door de beheerders. Bij constatering van misbruik wordt een waarschuwing gegeven of wordt het account gedeactiveerd.</li>
                 </ul>
                 <div class="checkbox">
                     <label>
-                        <input type="checkbox" name="terms" id="terms" required> Yes, ik ga hier mee akkoord!
+                        <input type="checkbox" name="terms" id="terms" required> Yes, ik ga hiermee akkoord!
                     </label>
                 </div>
             </div>
