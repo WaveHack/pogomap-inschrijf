a2f0bb0eb46f9fd528760ae1254b528c4ea10395
WaveHack <email@wavehack.net>
2017-03-06 11:37:35 +0100

    Remove navbar

--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -11,26 +11,6 @@
 </head>
 <body>
 
-<div class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{ route('register') }}">Inschrijf</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ route('register') }}">Inschrijven</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-
 <div class="container">
     @yield('content')
 </div>
