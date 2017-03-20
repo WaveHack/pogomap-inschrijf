7eba0feec240855c7d4120880a6f6d95a1f358e6
WaveHack <email@wavehack.net>
2017-03-21 00:47:38 +0100

    Change bold tag

--- app/resources/views/emails/registration/password-reset.blade.php
+++ app/resources/views/emails/registration/password-reset.blade.php
@@ -5,7 +5,7 @@ Een beheerder heeft je wachtwoord gereset op de {{ config('app.name') }}.
 
 Link: {{ url(env('MAP_URL')) }}<br>
 Gebruikersnaam: {{ $registration->username }}<br>
-<strong>Nieuwe wachtwoord: {{ $password }}</strong>
+<b>Nieuwe wachtwoord: {{ $password }}</b>
 
 @component('mail::button', ['url' => env('MAP_URL')])
 Ga naar de map
