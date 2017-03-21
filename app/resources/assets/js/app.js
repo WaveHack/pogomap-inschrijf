7272003485d14f6d30ddf52a4c7c03b996546d75
WaveHack <email@wavehack.net>
2017-03-21 10:40:35 +0100

    Don't Weedle inside textboxes

--- app/resources/assets/js/app.js
+++ app/resources/assets/js/app.js
@@ -5,8 +5,11 @@ require('./bootstrap');
 import Weedle from './weedle';
 
 cheet('w e e d l e', () => {
+    if (document.activeElement.tagName === 'INPUT') {
+        return;
+    }
+
     cheet.disable('w e e d l e');
 
-    let weedle = new Weedle;
-    weedle.run();
+    (new Weedle).run();
 });
